fdcdaa8a247abfccc0e457c7b02b6b3948127913
EllGuilherme <grds1000@gmail.com>
2022-03-04 22:33:01 -0300

    finalizar tarefa

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Clear();
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("pequena");
+Console.ResetColor();
+// cor branca na musica 
+Console.ForegroundColor = ConsoleColor .White;
+Console.WriteLine ("Deixa eu te contar, parei em seu olhar");
+Console.WriteLine ("Pequena");
+Console.WriteLine ("Vem, vou te mostrar");
+Console.WriteLine ("A lua, o céu e um mar de estrelas");
+Console.WriteLine ("Ela é a melhor que existe");
+Console.WriteLine ("Deixa belo o que era triste");
+Console.WriteLine ("Toda linda, ninguém resiste");
+Console.WriteLine ("Traz em si a calma");
+Console.WriteLine ("Teu sorriso é paz, leveza");
+Console.WriteLine ("Ama as flores e a natureza");
+Console.WriteLine ("Mas de toda a maior beleza");
+Console.WriteLine ("Traz na alma");
+Console.WriteLine();
+Console.ResetColor();
+//troca de cor
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine ("Deixa eu te contar, parei em seu olhar");
+Console.WriteLine ("Pequena");
+Console.WriteLine ("Vem, vou te mostrar");
+Console.WriteLine ("A lua, o céu e o mar de estrelas");
+Console.ResetColor();
+// cor branca da musica 
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine ("Deixa eu te contar, parei em seu olhar em seu olhar");
+Console.WriteLine ("Pequena");
+Console.WriteLine ("Vem, vou te mostrar");
+Console.WriteLine ("A lua, o céu e o mar (céu e mar) de estrelas");
+Console.WriteLine ("Deixa eu te contar, parei em seu olhar");
+Console.WriteLine ("Pequena");
+Console.WriteLine ("Vem, vou te mostrar");
+Console.WriteLine ("A lua, o céu e o mar de estrelas");
+Console.ResetColor();
+
+
+
+
